7f08baaf4797b4399dee6b33c90bc6293c44b1c3
Radhika K <multicoredump@gmail.com>
2026-03-07 19:40:58 -0800

    Build MomentEntryView with PhotoPicker, SwiftData container, and toolbar actions
    - Build Data Entry View - MomentsEntryView for adding a new Moment
    - Added a PhotoPicker
    - Created a Data Container - SwiftData
    - Added save and cancel features to MomentsEntry Toolbar

--- GratefulMoments/GratefulMomentsApp.swift
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct GratefulMomentsApp: App {
+    
+    let dataContainer = DataContainer()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(dataContainer)
         }
+        .modelContainer(dataContainer.modelContainer)
     }
 }
---
+++ GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -0,0 +1,18 @@
+//
+//  MomentsView.swift
+//  GratefulMoments
+//
+//  Created by Radhika Karandikar on 3/7/26.
+//
+
+import SwiftUI
+
+struct MomentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MomentsView()
+}
