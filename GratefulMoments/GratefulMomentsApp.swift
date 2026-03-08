719b7e0b7be825e7ab3406a16a87064874d586b3
Radhika K <multicoredump@gmail.com>
2026-03-07 18:41:19 -0800

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Radhika Karandikar on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
